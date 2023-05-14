5fbc2fb975778fb1e970af7280c3b82724303125
mirza-z <zaimovic330@gmail.com>
2023-05-14 17:46:59 +0200

    Added Hero, About and Support to page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Support from "./components/Support";
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
     </>
   );
 }
